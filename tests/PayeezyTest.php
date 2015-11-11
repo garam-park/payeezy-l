a489d968ff6d5d1151c9f8c16ba10459a62140f5
garam <netgarng88@gmail.com>
2015-11-11 01:06:37 -0500

    Add TestCase

--- tests/PayeezyTest.php
+++ tests/PayeezyTest.php
@@ -3,15 +3,51 @@
 * 
 */
 use Parkgaram\Payeezy\PayeezyApi;
+use Parkgaram\Payeezy\CreditCardPayments as CardPayments;
 
 class PayeezyTest extends PHPUnit_Framework_TestCase
 {
 	
-	public function testhelloTest()
+	// public function testhelloTest()
+ //    {
+ //    	$api = new PayeezyApi('1','2');
+ //    	$this->assertEquals($api->getApiKey(),'1');
+ //    	$this->assertEquals($api->getApiSecret(),'2');
+
+ //        $creditCartPayments->authorize();
+ //    }
+    
+  //   public function testCurl()
+  //   {
+  //   	$curl = new Curl\Curl();
+		// $curl->get('http://httpbin.org/get');
+
+		// var_dump($curl->request_headers);
+		// var_dump($curl->response_headers);
+  //   }
+
+    public function testCardPayments($value='')
     {
-    	$api = new PayeezyApi('1','2');
-    	$this->assertEquals($api->getApiKey(),'1');
-    	$this->assertEquals($api->getApiSecret(),'2');
+        $cardApi = new CardPayments(
+            );
+
+        $payload = [
+              "merchant_ref" => PayeezyApi::processInput('adf'),
+              "transaction_type" => PayeezyApi::processInput('purchase'),
+              "method" => PayeezyApi::processInput('credit_card'),
+              "amount" => PayeezyApi::processInput('1'),
+              "partial_redemption" => PayeezyApi::processInput('false'),
+              "currency_code" => PayeezyApi::processInput('USD'),
+              "credit_card" => [
+                "type" => PayeezyApi::processInput('visa'),
+                "cardholder_name" => PayeezyApi::processInput('John Smith'),
+                "card_number" => PayeezyApi::processInput('4788250000028291'),
+                "exp_date" => PayeezyApi::processInput('1020'),
+                "cvv" => PayeezyApi::processInput('123'),
+              ]
+        ];
+        
+        $response = $cardApi->purchase($payload);
+        var_dump($response);
     }
 }
-?>
